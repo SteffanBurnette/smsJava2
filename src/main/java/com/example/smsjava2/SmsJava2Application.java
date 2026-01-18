2bd3ce0135f5bee3cf13a880f27b770683631061
SteffanBurnette <burnettesteffansep7@gmail.com>
2026-01-18 12:58:41 -0500

    test

---
+++ src/main/java/com/example/smsjava2/SmsJava2Application.java
@@ -0,0 +1,13 @@
+package com.example.smsjava2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsJava2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsJava2Application.class, args);
+    }
+
+}
